0e9eef695d2837887ec85ead6321993c94620c42
netgridtech <netgridtech@gmail.com>
2026-05-01 14:35:44 +0100

    Create tailwind.config.js for NETGRID brand colors (navy and teal)

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        navy: '#001F3F',
+        teal: '#39CCCC',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
